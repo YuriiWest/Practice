85f8863eb9e45c92e8d4de7725263833d8e6073f
YuriiWest <43727292+YuriiWest@users.noreply.github.com>
2020-07-01 15:37:06 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,14 @@
+from math import *
+x = 1
+x = float(x)
+while True:
+    x += 0.2
+    y = (5*tan(x+7))/((x+3)**2)
+    if x >= 6.3:
+        if x > 6.3:
+            break
+    print ('y=', y, 'x=',x)
+
+
+
+
